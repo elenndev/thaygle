63019d830114e92c34bb324aa651be975a5e309f
Elen Damares <elen.damares774@gmail.com>
2024-12-19 18:36:12 -0300

    inicio ajuste layouts

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { useState } from "react";
+
+const HomeNavbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    return(<>
+    <nav className="absolute w-full top-[0] z-1 bg-[--devScheme-white] text-[--devScheme-gray]">
+        <div className="flex items-center justify-between p-4 lg:hidden">
+        <h1 className="text-xl font-bold">Logo</h1>
+        <button
+            aria-label="Toggle Navigation"
+            onClick={() => setIsOpen(!isOpen)}
+            className="focus:outline-none">
+        <span className="block w-8 h-1 bg-[--devScheme-gray] mb-1"></span>
+        <span className="block w-8 h-1 bg-[--devScheme-gray] mb-1"></span>
+        <span className="block w-8 h-1 bg-[--devScheme-gray]"></span>
+        </button>
+        </div>
+
+        <div className={`lg:flex lg:items-center lg:justify-between ${ isOpen ? "block" : "hidden"}`}>
+            <ul className="bg-[--devScheme-white] shadow flex absolute right-[0] flex-col lg:flex-row lg:gap-8 text-center">
+                <li className=" p-4 hover:bg-blue-700 lg:p-0">
+                    <a href="#home" className="text-[--devScheme-gray] font-bold">Contato</a>
+                </li>
+                <li className="p-4 hover:bg-blue-700 lg:p-0">
+                    <a href="#produtos" className="text-[--devScheme-gray] font-bold">Produtos</a>
+                </li>
+                <li className="p-4 hover:bg-blue-700 lg:p-0">
+                    <a href="#sobre" className="text-[--devScheme-gray] font-bold">Sobre</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    </>)
+}
+
+export default HomeNavbar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
 import Catalogo from "./components/Catalogo";
+import HomeNavbar from "./components/Navbar";
 
-export default function Home() {
+const Home = () => {
+  return(<>
+    <div className="home flex overflow-x-hidden flex-col items-center justify-center h-full w-full relative bg-[--devScheme-blue]" id="home">
+      <h1 className="text-[--devScheme-orange] text-[2rem] uppercase">Churrasqueiras pré-moldadas</h1>
+      <span className="w-fit bg-[--devScheme-white] text-[--devScheme-gray] py-[5px] px-[20px] rounded-[10px]">
+        <a className="localizacao">Em Itumbiara - GO e proximidades</a>
+      </span>
+    </div>
+  </>)
+}
+
+export default function HomePage() {
   if (typeof window === "undefined"){
     console.log("server")
   }else{
@@ -9,7 +21,9 @@ export default function Home() {
 
   return (
     <>
-    <div className="min-h-[fit-content] bg-gray-900 w-full h-full flex flex-col items-center justify-center">
+    <div className="homepage relative min-h-[fit-content] bg-gray-900 w-screen h-full flex flex-col items-center justify-end">
+      <Home />
+      <HomeNavbar />
       <Catalogo />
     </div>
     </>
